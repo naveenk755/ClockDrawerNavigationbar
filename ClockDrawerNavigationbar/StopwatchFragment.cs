3dea83fdf612ef5b87c3081334fe2c6c7b5bee72
naveenk755 <naveenk755@outlook.com>
2017-07-08 22:53:44 +0530

    created fragments.

---
+++ ClockDrawerNavigationbar/StopwatchFragment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System.Timers;
+
+namespace ClockDrawerNavigationbar
+{
+    public class StopwatchFragment : Fragment
+    {
+        Timer timer;
+        TextView timeTextView;
+        TimeSpan ticks;
+        Button StartStopButton;
+        Button ResetButton;
+
+        public StopwatchFragment()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += OnElapsed;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Stopwatch, container, false);
+
+            timeTextView = view.FindViewById<TextView>(Resource.Id.stopwatchTimeTextView);
+
+            StartStopButton = view.FindViewById<Button>(Resource.Id.StartStopButton);
+            StartStopButton.Click += OnStartStopClick;
+            ResetButton = view.FindViewById<Button>(Resource.Id.ResetButton);
+            ResetButton.Click += OnResetClick;
+
+            return view;
+        }
+
+        void OnStartStopClick(object sender, EventArgs e)
+        {
+            if(StartStopButton.Text=="Start")
+            {
+                timer.Start();
+                StartStopButton.Text = "Stop";
+            }
+            else
+            {
+                timer.Stop();
+                StartStopButton.Text = "Start";
+            }
+        }
+
+        void OnResetClick(object sender, EventArgs e)
+        {
+            ticks = TimeSpan.Zero;
+            timer.Stop();
+            StartStopButton.Text = "Start";
+            timeTextView.Text = "0:00:00";
+        }
+
+        void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            ticks = ticks.Add(TimeSpan.FromSeconds(1));
+            timeTextView.Text = ticks.ToString("g");
+        }
+    }
+}
---
+++ ClockDrawerNavigationbar/TimeFragment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System.Timers;
+
+namespace ClockDrawerNavigationbar
+{
+    public class TimeFragment : Fragment
+    {
+        Timer timer;
+        TextView timeTextView;
+
+        public TimeFragment()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += OnElapsed;
+            timer.Start();
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Time, container, false);
+
+            timeTextView = view.FindViewById<TextView>(Resource.Id.timeTextView);
+            timeTextView.Text = DateTime.Now.ToString("T");
+
+            return view;
+        }
+
+        void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            timeTextView.Text = DateTime.Now.ToString("T");
+        }
+    }
+}
